dcb7356859e407c77108a42735fc97a6ab3732f2
Fernando Isaguirre <fernando.isaguirre.13@gmail.com>
2023-01-04 22:06:15 -0300

    docker compose

--- user-parking-control-service/app.py
+++ user-parking-control-service/app.py
@@ -1,14 +1,42 @@
 # Import flask module
 from flask import Flask
+import re
+import jwt
+from functools import wraps
+
+from database_conexion import obtener_conexion
+
+#from flask_mysqldb import MySQL
+#import MySQLdb.cursors
+
 
 app = Flask(__name__)
+
+obtener_conexion(app)
+
+#mysql = MySQL(app)
+
+SECRET_KEY = 'your secret key'
 
 
 @app.route('/')
 def index():
-    return 'Hello to Flask- This is user-parking-control-service!'
+    # return 'Hello to Flask- This is user-parking-control-service!'
+    return 'Hello to Flask!Hello tossssss dd!'
 
 
+"""
+@app.route('/users')
+def user():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM usuario')
+    user = cursor.fetchone()
+
+    cursor.close()
+
+    return jsonify(user)
+
+"""
 # main driver function
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ user-parking-control-service/database_conexion.py
@@ -0,0 +1,25 @@
+import pymysql
+"""
+def obtener_conexion():
+    return pymysql.connect(host='db',
+                                user='root',
+                                password='root',
+                                port = 3306,
+                                db='podcast')
+"""
+"""
+def obtener_conexion():
+    return pymysql.connect(host='localhost',
+                                user='fprueba',
+                                password='Fernandoroot2022.',
+                                port = 3306,
+                                db='podcast')
+"""
+
+
+def obtener_conexion(app):
+    app.config['SECRET_KEY'] = 'your secret key'
+    app.config['MYSQL_HOST'] = 'lo'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'Fernandoroot2023.'
+    app.config['MYSQL_DB'] = 'userparkingcontrol'
