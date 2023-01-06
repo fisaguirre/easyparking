b08fdc6186a24857f4e4c34ad71af71cebe62a2c
Fernando Isaguirre <fernando.isaguirre.13@gmail.com>
2023-01-06 19:55:09 -0300

    sign up

--- user-parking-control-service/app.py
+++ user-parking-control-service/app.py
@@ -1,10 +1,11 @@
 # Import flask module
-from flask import Flask
+from flask import Flask, jsonify, request
 import re
 import jwt
 from functools import wraps
-
-#from database_conexion import obtener_conexion
+from database_conexion import obtener_conexion
+from controller import signup
+from controller import signin
 
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
@@ -12,19 +13,46 @@ import MySQLdb.cursors
 
 app = Flask(__name__)
 
-# obtener_conexion(app)
+obtener_conexion(app)
 
-
+"""
 app.config['SECRET_KEY'] = 'your secret key'
-app.config['MYSQL_HOST'] = '172.0.0.1'
+app.config['MYSQL_HOST'] = 'db_user_parking_control_service'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Fernandoroot2023.'
-app.config['MYSQL_PORT'] = 3306
-app.config['MYSQL_DB'] = 'userparkingcontro'
-
+app.config['MYSQL_DB'] = 'userparkingcontrol'
+"""
 mysql = MySQL(app)
 
 #SECRET_KEY = 'your secret key'
+
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message': 'a valid token is missing'})
+        try:
+            data = jwt.decode(
+                token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            #current_user = Users.query.filter_by(public_id=data['public_id']).first()
+
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute(
+                'SELECT * FROM usuario WHERE usuario.username = "admin1"')
+            current_user = cursor.fetchone()
+
+            cursor.close()
+
+        except:
+            return jsonify({'message': 'token is invalid'})
+
+        return f(current_user, *args, **kwargs)
+    return
 
 
 @app.route('/')
@@ -33,6 +61,24 @@ def index():
     return 'a sfffss!'
 
 
+@app.route('/signup', methods=['POST'])
+def register():
+    return signup.register(request, mysql, app)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    # creates dictionary of form data
+    return signin.login(request.form, mysql, app)
+
+
+@app.route('/asignarRol')
+def getUsers():
+
+    return 'asignar rol admin o tarjetero'
+
+
+"""
 @app.route('/users')
 def getUsers():
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -42,7 +88,7 @@ def getUsers():
     cursor.close()
 
     return jsonify(user)
-
+"""
 
 """
 @app.route('/users')
---
+++ user-parking-control-service/controller/signup.py
@@ -0,0 +1,127 @@
+from datetime import timedelta
+from datetime import datetime, date, timedelta
+from flask import jsonify, request
+
+import re
+import uuid  # for public id
+from werkzeug.security import generate_password_hash, check_password_hash
+# imports for PyJWT authentication
+import jwt
+from functools import wraps
+
+from database_conexion import obtener_conexion
+from model.user import User
+
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
+
+from model.user import User
+import MySQLdb
+from flask import make_response
+
+
+def register(request, mysql, app):
+    print("asd")
+
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        user_signup_data = request.form
+
+        new_password = generate_password_hash(user_signup_data.get('password'))
+        new_password2 = generate_password_hash(
+            user_signup_data.get('password'))
+        new_password3 = generate_password_hash(
+            user_signup_data.get('password'))
+
+        print("1: ", new_password)
+        print("2: ", new_password2)
+        print("3: ", new_password3)
+        new_user = User(str(uuid.uuid4()), user_signup_data.get('username'),  new_password,
+                        user_signup_data.get('nombre'), user_signup_data.get('apellido'), user_signup_data.get('email'), 'NULL')
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+        cursor.execute(
+            'SELECT * FROM usuario WHERE usuario.username = %s', (new_user.get_username(),))
+        user_exists = cursor.fetchone()
+        cursor.close()
+
+        if user_exists:
+            # returns 202 if user already exists
+            response = make_response(
+                jsonify(
+                    {"message": 'User already exists. Please Log in'}
+                ),
+                202,
+            )
+            response.headers["Content-Type"] = "application/json"
+            response.headers["WWW-Authenticate"] = "User already exists!!"
+            return response
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            'SELECT * FROM usuario WHERE usuario.email = %s', (new_user.get_email(),))
+        email_exists = cursor.fetchone()
+        cursor.close()
+        if email_exists:
+            # returns 202 if user already exists
+            response = make_response(
+                jsonify(
+                    {"message": 'User with that email already exists. Please Log in'}
+                ),
+                202,
+            )
+            response.headers["Content-Type"] = "application/json"
+            response.headers["WWW-Authenticate"] = "User already exists!!"
+            return response
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', user_signup_data.get('email')):
+            response = make_response(
+                jsonify(
+                    {"message": 'Invalid email address !'}
+                ),
+                400,
+            )
+            response.headers["Content-Type"] = "application/json"
+            response.headers["WWW-Authenticate"] = "Invalid email address !"
+            return response
+        elif not re.match(r'[A-Za-z0-9]+', user_signup_data.get('username')):
+            response = make_response(
+                jsonify(
+                    {"message": 'Username must contain only characters and numbers !'}
+                ),
+                400,
+            )
+            response.headers["Content-Type"] = "application/json"
+            response.headers["WWW-Authenticate"] = "Username must contain only characters and numbers !"
+            return response
+        # signup new User with user rol
+        else:
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+            data = (new_user.get_public_id(),
+                    new_user.get_username(), new_user.get_password(
+            ), new_user.get_nombre(), new_user.get_apellido(),
+                new_user.get_email(), new_user.get_rol())
+
+            cursor.execute(
+                "insert into usuario values(NULL, %s, %s, %s, %s, %s, %s, %s)", data)
+            mysql.connection.commit()
+            cursor.close()
+
+            response = make_response(
+                jsonify(
+                    {"message": 'Successfully registered'}
+                ),
+                201,
+            )
+            response.headers["Content-Type"] = "application/json"
+            return response
+    elif request.method == 'POST':
+        response = make_response(
+            jsonify(
+                {"message": 'Please fill out the form !'}
+            ),
+            201,
+        )
+        response.headers["Content-Type"] = "application/json"
+        response.headers["WWW-Authenticate"] = "Please fill out the form !"
+        return response
