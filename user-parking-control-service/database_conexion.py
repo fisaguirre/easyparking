e5d78ad1e6ae72d76b8e5e9e709d4fbab4f44485
Fernando Isaguirre <fernando.isaguirre.13@gmail.com>
2023-01-06 19:55:17 -0300

    sign in with token

--- user-parking-control-service/database_conexion.py
+++ user-parking-control-service/database_conexion.py
@@ -19,7 +19,7 @@ def obtener_conexion():
 
 def obtener_conexion(app):
     app.config['SECRET_KEY'] = 'your secret key'
-    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_HOST'] = 'db_user_parking_control_service'
     app.config['MYSQL_USER'] = 'root'
     app.config['MYSQL_PASSWORD'] = 'Fernandoroot2023.'
     app.config['MYSQL_DB'] = 'userparkingcontrol'
