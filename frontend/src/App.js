741f273995a65a72de8027cf255ae1cf5fb49ffe
Fernando Isaguirre <fernando.isaguirre.13@gmail.com>
2023-01-14 03:40:19 -0300

    Estilo formulario login y registro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,26 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home"
 import SignUp from "./pages/SignUp"
 import Login from "./pages/Login"
+import Users from "./pages/Users";
 import Navbar from "./Navbar";
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+
+      <BrowserRouter>
+        <Navbar />
+        <div className="container p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/users" className="auto" element={<Users />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
