344796e28480b03a9e7d598e8a0e9803d4061cd1
Fernando Isaguirre <fernando.isaguirre.13@gmail.com>
2023-01-15 21:51:36 -0300

    FrontEnd - DIseño estructura con botones tarjeta digital

--- frontend/src/components/parking-control/tarjetero/DisenioTarjeta.jsx
+++ frontend/src/components/parking-control/tarjetero/DisenioTarjeta.jsx
@@ -3,10 +3,295 @@ import React from "react";
 export default function DisenioTarjeta() {
     return (
         <div>
-            <h1>This is Diseño de la tarjeta con los botones y sus funciones</h1>
-            <div>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2"><h3>mendoza ciudad</h3>
+                </div>
+                <div className="col-md-2"></div>
+                <div className="col-md-1">
+                </div>
+                <div className="col-md-2"><h4>Programa de estacionamiento medido<br></br>2023</h4>
+                </div>
+            </div>
+            <p></p>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-6">
+                    <h5>Pago y uso obligatorio al momento de estacionar</h5>
+                </div>
                 <p></p>
-                <h3>Aca va la imagen de la tarjeta con botones y sus funciones</h3>
+
+            </div>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2"><h4>Patente</h4>
+                </div>
+                <div className="col-md-2"></div>
+                <div className="col-md-2"><h4>Mes</h4>
+                </div>
+                <div className="col-md-2">
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">0</button>&nbsp;&nbsp;
+                    <button type="button">0</button>
+                </div>
+                <div className="col-md-2">
+                    <button type="button">Enero</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-2">
+                    <button type="button">Febrero</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-2">
+                    <button type="button">Marzo</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-2"></div>
+            </div>
+            <p></p>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">1</button>&nbsp;&nbsp;
+                    <button type="button">1</button>
+                </div>
+                <div className="col-md-1">
+                </div>
+                <div className="col-md-4">Valido hasta el 31/10/2023<br></br>Dia
+                </div>
+                <div className="col-md-2">
+                </div>
+                <div className="col-md-2"></div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">2</button>&nbsp;&nbsp;
+                    <button type="button">2</button>
+                </div>
+                <div className="col-md-2">
+                    <button type="button">Lunes</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-2">
+                    <button type="button">Martes</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-2">
+                    <button type="button">Miercoles</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-2"></div>
+            </div>
+            <p></p>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">3</button>&nbsp;&nbsp;
+                    <button type="button">3</button>
+                </div>
+                <div className="col-md-2">
+                    <button type="button">Jueves</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-2">
+                    <button type="button">Viernes</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-2">
+                    <button type="button">Sabado</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-2"></div>
+            </div>
+            <p></p>
+
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">4</button>&nbsp;&nbsp;
+                    <button type="button">4</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">1</button>&nbsp;&nbsp;
+                    <button type="button">2</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">3</button>&nbsp;&nbsp;
+                    <button type="button">4</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">5</button>&nbsp;&nbsp;
+                    <button type="button">6</button>
+                </div>
+            </div>
+            <p></p>
+
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">5</button>&nbsp;&nbsp;
+                    <button type="button">5</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">6</button>&nbsp;&nbsp;
+                    <button type="button">7</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">8</button>&nbsp;&nbsp;
+                    <button type="button">10</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">11</button>&nbsp;&nbsp;
+                    <button type="button">12</button>
+                </div>
+            </div>
+            <p></p>
+
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">6</button>&nbsp;&nbsp;
+                    <button type="button">6</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">13</button>&nbsp;&nbsp;
+                    <button type="button">14</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">15</button>&nbsp;&nbsp;
+                    <button type="button">16</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">17</button>&nbsp;&nbsp;
+                    <button type="button">18</button>
+                </div>
+            </div>
+            <p></p>
+
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">7</button>&nbsp;&nbsp;
+                    <button type="button">7</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">19</button>&nbsp;&nbsp;
+                    <button type="button">20</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">21</button>&nbsp;&nbsp;
+                    <button type="button">22</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">23</button>&nbsp;&nbsp;
+                    <button type="button">24</button>
+                </div>
+            </div>
+            <p></p>
+
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">8</button>&nbsp;&nbsp;
+                    <button type="button">8</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">25</button>&nbsp;&nbsp;
+                    <button type="button">26</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">27</button>&nbsp;&nbsp;
+                    <button type="button">28</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">29</button>&nbsp;&nbsp;
+                    <button type="button">30</button>
+                </div>
+            </div>
+            <p></p>
+
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">9</button>&nbsp;&nbsp;
+                    <button type="button">9</button>
+                </div>
+                <div className="col-md-1">
+                    <button type="button">31</button>&nbsp;&nbsp;
+                </div>
+            </div>
+
+            <p></p>
+            <p></p>
+            <p></p>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2"><h4>Hora</h4>
+                </div>
+                <div className="col-md-2"></div>
+                <div className="col-md-2"><h4>Minutos</h4>
+                </div>
+                <div className="col-md-2">
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">8</button>&nbsp;&nbsp;
+                    <button type="button">9</button>&nbsp;&nbsp;
+                    <button type="button">10</button>&nbsp;&nbsp;
+                    <button type="button">11</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-4">
+                    <button type="button">00</button>&nbsp;&nbsp;
+                    <button type="button">05</button>&nbsp;&nbsp;
+                    <button type="button">10</button>&nbsp;&nbsp;
+                    <button type="button">15</button>&nbsp;&nbsp;
+                </div>
+            </div>
+            <p></p>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">12</button>&nbsp;&nbsp;
+                    <button type="button">13</button>&nbsp;&nbsp;
+                    <button type="button">14</button>&nbsp;&nbsp;
+                    <button type="button">15</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-4">
+                    <button type="button">20</button>&nbsp;&nbsp;
+                    <button type="button">25</button>&nbsp;&nbsp;
+                    <button type="button">30</button>&nbsp;&nbsp;
+                    <button type="button">35</button>&nbsp;&nbsp;
+                </div>
+            </div>
+            <p></p>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">16</button>&nbsp;&nbsp;
+                    <button type="button">17</button>&nbsp;&nbsp;
+                    <button type="button">18</button>&nbsp;&nbsp;
+                    <button type="button">19</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-4">
+                    <button type="button">40</button>&nbsp;&nbsp;
+                    <button type="button">45</button>&nbsp;&nbsp;
+                    <button type="button">50</button>&nbsp;&nbsp;
+                    <button type="button">55</button>&nbsp;&nbsp;
+                </div>
+            </div>
+            <p></p>
+            <div className="row">
+                <div className="col-md-2"></div>
+                <div className="col-md-2">
+                    <button type="button">20</button>&nbsp;&nbsp;
+                </div>
+                <div className="col-md-4">
+                </div>
+            </div>
+            <p></p>
+
+
+            <div>
+
                 <p></p>
                 <button type="button" id="signup-button" className="btn btn-info">Activar tarjeta</button>
             </div>
@@ -30,6 +315,6 @@ export default function DisenioTarjeta() {
                 <p></p>
                 <button type="button" id="signup-button" className="btn btn-info">Volver</button>
             </div>
-        </div>
+        </div >
     )
 }
