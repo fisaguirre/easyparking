be4ec01d72becfaf37a4426c7aa9a987555d5ca0
Fernando Isaguirre <fernando.isaguirre.13@gmail.com>
2023-04-21 19:51:50 -0300

    FrontEnd - Mostrar log de tarjetas pagadas

--- frontend/src/components/payment/PagoEfectuado.jsx
+++ frontend/src/components/payment/PagoEfectuado.jsx
@@ -1,8 +1,18 @@
-import React, { useState, useEffect } from "react";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import "./styles/pagoEfectuado.css";
+import { useState, useEffect } from "react";
 const API_PAYMENT = process.env.REACT_APP_API_PAYMENT;
 
 
 export default function PagoEfectuado() {
+
     let [pagosEfectuados, setPagosEfectuados] = useState([]);
 
     const usuario_id = sessionStorage.getItem("usuario_id")
@@ -26,35 +36,48 @@ export default function PagoEfectuado() {
     }, [usuario_id]);
 
     return (
-        <div id="form-text" className="row">
-            <h3>Tarjetas pagadas por conductores</h3>
-            <div className="col-md-6">
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Fecha</th>
-                            <th>Precio</th>
-                            <th>Tiempo</th>
-                            <th>Tarjetas</th>
-                            <th>Patente</th>
-                            <th>Usuario MP</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+        <div className="Table">
+            <h3>Tarjetas abonadas por conductores</h3>
+            <TableContainer
+                component={Paper}
+                style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
+            >
+                <Table className="tableBody" sx={{ minWidth: 700 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Fecha</TableCell>
+                            <TableCell align="left">Precio</TableCell>
+                            <TableCell align="left">Tiempo</TableCell>
+                            <TableCell align="left">Tarjetas</TableCell>
+                            <TableCell align="left">Patente</TableCell>
+                            <TableCell align="left">Usuario MP</TableCell>
+                            <TableCell align="left"></TableCell>
+                        </TableRow>
+                    </TableHead>
+
+
+                    {/*<TableBody style={{ color: "white" }}>*/}
+                    <TableBody >
                         {pagosEfectuados.map((pago) => (
-                            <tr key={pago.pago_id}>
-                                <td>{pago.fecha}</td>
-                                <td>${pago.precio_total}</td>
-                                <td>{pago.tiempo_total} min</td>
-                                <td>{pago.cantidad_tarjetas}</td>
-                                <td>{pago.patente}</td>
-                                <td>{pago.username_mercado}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                    <p></p>
-                </table>
-            </div>
+                            <TableRow
+                                key={pago.pago_id}
+                                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                            >
+                                <TableCell align="left" component="th" scope="row">
+                                    {pago.fecha}
+                                </TableCell>
+                                <TableCell align="left">${pago.precio_total}</TableCell>
+                                <TableCell align="left">{pago.tiempo_total} min</TableCell>
+                                <TableCell align="left">{pago.cantidad_tarjetas}</TableCell>
+                                <TableCell align="left">{pago.patente}</TableCell>
+                                <TableCell align="left">{pago.username_mercado}</TableCell>
+                                {/*<TableCell align="left" className="Details">Details</TableCell>*/}
+                            </TableRow>
+                        ))
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </div>
     );
-};
+}
